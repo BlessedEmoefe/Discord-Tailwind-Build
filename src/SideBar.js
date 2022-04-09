1871691ca1efc8149b5ea54d242573e6c047bed8
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2022-04-09 22:57:26 +0100

    installed react-icons

--- src/SideBar.js
+++ src/SideBar.js
@@ -3,11 +3,16 @@ import React from "react";
 export const Sidebar = () => {
   return (
     <div className="fixed top-0 left-0 w-16 h-screen bg-gray-900 flex flex-col text-white shadow">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+    <SidebarIcon icon={}  />
     </div>
   );
 };
+
+
+const SidebarIcon =({icon})=>{
+    return(
+<div className="sidebar-icon">
+{icon}
+</div>
+    )
+}
