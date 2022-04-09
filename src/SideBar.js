0a531168c375ef5963aa0b820b25227175030131
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2022-04-09 23:21:54 +0100

    .

--- src/SideBar.js
+++ src/SideBar.js
@@ -5,11 +5,17 @@ import { FaFire, FaPoo } from "react-icons/fa";
 export const Sidebar = () => {
   return (
     <div className="fixed top-0 left-0 w-16 h-screen bg-gray-900 flex flex-col text-white shadow">
-      <SidebarIcon icon={<FaFire size="28" />} />
+      <SideBarIcon icon={<FaFire size="28" />} />
+      {/* <Divider /> */}
+      <SideBarIcon icon={<BsPlus size="32" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+      {/* <Divider /> */}
+      <SideBarIcon icon={<BsGearFill size="22" />} />
     </div>
   );
 };
 
-const SidebarIcon = ({ icon }) => {
+const SideBarIcon = ({ icon }) => {
   return <div className="sidebar-icon">{icon}</div>;
 };
